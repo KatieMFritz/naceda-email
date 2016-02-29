47ab000ad8bd7f3045de4dd4d4d18aa2f0630978
Chris Fritz <chrisvfritz@gmail.com>
2016-02-29 10:52:35 -0500

    stripped STRIP comments from HTML

--- template-generator/app.rb
+++ template-generator/app.rb
@@ -30,7 +30,8 @@ class NacedaEmailTemplateGenerator < Sinatra::Base
 
     def emailify template
       document = Roadie::Document.new(
-        erb :"emails/#{template}", layout: :'layouts/email'
+        erb(:"emails/#{template}", layout: :'layouts/email')
+          .gsub(/<!--\s*STRIP:.+?-->/, '')
       )
       document.asset_providers = [
         Roadie::FilesystemProvider.new(File.dirname(__FILE__) + '/public/')
